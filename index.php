dc28a86affac01230223b9a5745531f993ce7d5c
Shakir <shakir.salman.eng@gmail.com>
2021-05-11 19:55:30 +0200

    Display a posts
    Display a posts in index.php by loop the post and show excerpt of content.

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
 <?php
-	get_header();
+get_header();
 ?>
 
-	<h1>Hello World!</h1>
+<main class="container mt-3">
+
+	<pre>index.php</pre>
+
+	<hr />
+
+	<div class="row">
+		<div class="col-md-9 content">
+			<!-- Do we have any posts to display? -->
+			<?php if (have_posts()) : ?>
+				<!-- Yay, we has posts do display! -->
+				<?php while (have_posts()) : ?>
+					<!-- Start post -->
+					<?php
+						// Load next post to display
+						the_post();
+						get_template_part('template-parts/content', 'excerpt');
+					?>
+					<!-- End post -->
+				<?php endwhile; ?>
+			<?php else: ?>
+				<p>Sorry, no posts found.</p>
+			<?php endif; ?>
+		</div><!-- /.col-md-9 -->
+
+	</div><!-- /.row -->
+</main><!-- /.container -->
 
 <?php
-	get_footer();
+get_footer();
