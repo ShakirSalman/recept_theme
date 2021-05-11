eff5911267e9676a1a385a3f967957eb9e91ad06
Shakir <shakir.salman.eng@gmail.com>
2021-05-11 20:15:48 +0200

    Display a post
    Display a post in single.php by show the content.

---
+++ single.php
@@ -0,0 +1,34 @@
+<?php
+get_header();
+?>
+
+<main class="container">
+
+	<pre>single.php</pre>
+
+	<hr />
+
+	<div class="row">
+		<div class="col-md-9 content">
+			<!-- Do we have any posts to display? -->
+			<?php if (have_posts()) : ?>
+				<!-- Yay, we has posts do display! -->
+				<?php while (have_posts()) : ?>
+					<!-- Start post -->
+					<?php
+						// Load next post to display
+						the_post();
+						get_template_part('template-parts/content');
+					?>
+					<!-- End post -->
+				<?php endwhile; ?>
+			<?php else: ?>
+				<p>Sorry, no post found.</p>
+			<?php endif; ?>
+		</div><!-- /.col-md-9 -->
+
+	</div><!-- /.row -->
+</main><!-- /.container -->
+
+<?php
+get_footer();
