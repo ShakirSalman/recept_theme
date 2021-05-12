15b4a999c30be8675b1736fceb4fe593b79ae28d
Shakir <shakir.salman.eng@gmail.com>
2021-05-12 18:26:49 +0200

    Adding support for title-tag.

--- header.php
+++ header.php
@@ -9,8 +9,6 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
 	integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
-	<title><?php bloginfo('name') ?></title>
-
 	<?php wp_head(); ?>
 </head>
 <body>
