bdb56501e2fd10ca1d72c34fb7c1189b8a68ce5e
Shakir <shakir.salman.eng@gmail.com>
2021-05-12 18:37:49 +0200

    Adding blog sidebar functionality.

--- functions.php
+++ functions.php
@@ -17,10 +17,10 @@ add_filter('excerpt_length', 'recept_theme_excerpt_length', 10, 1);
  * @param string $suffix
  * @return string
  */
-function mbt_filter_excerpt_more($suffix) {
+function recept_theme_filter_excerpt_more($suffix) {
 	return "...";
 }
-add_filter('excerpt_more', 'mbt_filter_excerpt_more');
+add_filter('excerpt_more', 'recept_theme_filter_excerpt_more');
 
 /**
  * Append a 'Read more'-button to excerpts.
@@ -28,12 +28,30 @@ add_filter('excerpt_more', 'mbt_filter_excerpt_more');
  * @param string $excerpt
  * @return string
  */
-function mbt_filter_the_excerpt($excerpt) {
+function recept_theme_filter_the_excerpt($excerpt) {
 	return $excerpt . '<div><a href="' . get_the_permalink() . '" class="btn btn-primary">Read more &raquo;</a></div>';
 }
-add_filter('the_excerpt', 'mbt_filter_the_excerpt');
+add_filter('the_excerpt', 'recept_theme_filter_the_excerpt');
 
 /**
  * Declare support for 'title-tag'
  */
  add_theme_support('title-tag');
+ 
+ /**
+ * Register widget areas (sidebars).
+ *
+ * @return void
+ */
+function recept_theme_widgets_init() {
+	register_sidebar([
+		'name' => 'Blog Sidebar',
+		'id' => 'blog-sidebar',
+		'description' => 'Sidebar on blog index, category archive and single blog posts.',
+		'before_widget' => '<div id="%1$s" class="card mb-3 widget %2$s"><div class="card-body">',
+		'after_widget' => '</div></div>',
+		'before_title' => '<h3 class="widget-title h5">',
+		'after_title' => '</h3>',
+	]);
+}
+add_action('widgets_init', 'recept_theme_widgets_init');
