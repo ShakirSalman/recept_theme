1a5c2d4e0444fa69843870e9736cf1ebb64f8e20
Shakir <shakir.salman.eng@gmail.com>
2021-05-16 23:35:05 +0200

    adding widgets in footer.

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<footer class="site-footer mt-3 py-3 bg-light text-dark">
+	<?php get_sidebar('footer'); ?>
+</footer>
+
 <!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
 	integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
