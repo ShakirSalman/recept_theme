278e7b9f471e832f2bc2d303ccef3fd40a90e4ba
Shakir <shakir.salman.eng@gmail.com>
2021-05-17 02:24:30 +0200

    Adding 404 page, Search result, blog home.

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+get_header();
+?>
+
+<main class="container mt-3">
+
+	<h1>Page Not Found</h1>
+
+	<pre>404.php</pre>
+
+	<hr />
+
+	<div class="text-center mb-5">
+		<img src="https://blog.thomasnet.com/hs-fs/hubfs/shutterstock_774749455.jpg?width=600&name=shutterstock_774749455.jpg" class="img-fluid" alt="A cat hiding under a stack of papers" title="Peek-a-boo!">
+
+		<p class="display-6 w-50 ms-auto me-auto mt-5 mb-5">Sorry, this page is not found! </p>
+
+		<p>Maybe try searching?</p>
+
+		<?php get_search_form(); ?>
+	</div>
+</main><!-- /.container -->
+
+<?php
+get_footer();
---
+++ template-parts/content-search.php
@@ -0,0 +1,9 @@
+<li>
+	<a href="<?php the_permalink(); ?>">
+		<?php the_title(); ?>
+	</a>
+	
+	<br/>
+	
+	<?php the_excerpt(); ?>
+</li>
